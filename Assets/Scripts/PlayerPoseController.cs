c5fad23b9d4071d5d2ace14dca9b4850dd03b576
Aliffathur <aliffathurzakaria@gmail.com>
2025-09-15 16:59:55 +0700

    Pembetulan Pose Prefab

--- Assets/Scripts/PlayerPoseController.cs
+++ Assets/Scripts/PlayerPoseController.cs
@@ -1,63 +1,61 @@
-using UnityEngine;
 using System;
-using System.Collections.Generic;
+using UnityEngine;
 
-public enum PoseType { PoseA=0, PoseB=1, PoseC=2, PoseD=3 }
-
-[RequireComponent(typeof(Animator))]
 public class PlayerPoseController : MonoBehaviour
 {
-    [Header("Animator (opsional)")]
-    public string animatorPoseParam = "PoseIndex";
-    private Animator animator;
+    public GameObject[] posePrefabs;  // Array untuk 4 pose prefab
+    private GameObject currentPoseInstance;
+    private int currentPoseIndex = 0; // Menggunakan index untuk pose
+    public KeyCode switchPoseKey = KeyCode.E;   // Tombol untuk ganti pose
 
-    [Header("Input")]
-    public KeyCode switchPoseKey = KeyCode.E;
-
-    [Header("Hitboxes (A–D)")]
-    public List<Collider> poseHitboxes;
-
-    [Header("Single Visual Root (opsional)")]
-    public Transform visualRoot;
-
-    [Serializable]
-    public struct PoseTransform {
-        public Vector3 localPosition;
-        public Vector3 localEulerAngles;
-        public Vector3 localScale;
-    }
-    [Header("Transforms per Pose (A–D)")]
-    public PoseTransform[] poseTransforms = new PoseTransform[4];
-
-    public PoseType currentPose = PoseType.PoseA;
-
-    void Awake() { animator = GetComponent<Animator>(); ApplyPose(); }
-
-    void Update() { if (Input.GetKeyDown(switchPoseKey)) CyclePose(); }
-
-    public void CyclePose() {
-        currentPose = (PoseType)(((int)currentPose + 1) % 4);
-        ApplyPose();
+    void Start()
+    {
+        // Set Pose awal (misalnya PoseA)
+        if (posePrefabs.Length > 0)
+        {
+            SetPose(currentPoseIndex); 
+        }
+        else
+        {
+            Debug.LogError("No Pose Prefabs assigned!");
+        }
     }
 
-    void ApplyPose() {
-        // Animator (jika dipakai)
-        if (animator) animator.SetInteger(animatorPoseParam, (int)currentPose);
+    void Update()
+    {
+        if (Input.GetKeyDown(switchPoseKey))
+        {
+            CyclePose();
+        }
+    }
 
-        // Hitbox on/off
-        for (int i = 0; i < poseHitboxes.Count; i++) {
-            if (!poseHitboxes[i]) continue;
-            bool on = (i == (int)currentPose);
-            poseHitboxes[i].enabled = on;
-            poseHitboxes[i].gameObject.SetActive(on);
+    // Fungsi untuk mengganti pose
+    public void CyclePose()
+    {
+        Debug.Log("Switching Pose");
+        currentPoseIndex = (currentPoseIndex + 1) % posePrefabs.Length; // Update pose index dengan modulo
+        Debug.Log("Next Pose Index: " + currentPoseIndex);
+        SetPose(currentPoseIndex); // Ganti pose sesuai index
+    }
+
+    // Fungsi untuk set pose sesuai index
+    void SetPose(int poseIndex)
+    {
+        // Hapus pose sebelumnya
+        if (currentPoseInstance != null)
+        {
+            Destroy(currentPoseInstance);
         }
 
-        // Ubah tampilan 1 mesh (pos/rot/scale)
-        if (visualRoot && poseTransforms != null && poseTransforms.Length >= 4) {
-            var t = poseTransforms[(int)currentPose];
-            visualRoot.localPosition = t.localPosition;
-            visualRoot.localEulerAngles = t.localEulerAngles;
-            visualRoot.localScale = t.localScale;
+        // Instantiate prefab pose yang baru
+        if (poseIndex >= 0 && poseIndex < posePrefabs.Length)
+        {
+            currentPoseInstance = Instantiate(posePrefabs[poseIndex], transform.position, transform.rotation);
+            currentPoseInstance.transform.SetParent(transform); // Set parent untuk sinkronisasi posisi
+        }
+        else
+        {
+            Debug.LogError("Invalid pose index!");
         }
     }
 }
